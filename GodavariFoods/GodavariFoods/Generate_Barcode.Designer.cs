90a0ad28d0d057f9351a72b408f67264058fd4ee
Rajesh Kalidindi <Rajesh Kalidindi>
2015-10-21 23:26:30 -0500

    initial commit

---
+++ GodavariFoods/GodavariFoods/Generate_Barcode.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GodavariFoods
+{
+    partial class Generate_Barcode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.pbBarcode = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBarcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(310, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Barcode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBarcode.Location = new System.Drawing.Point(46, 12);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(243, 23);
+            this.txtBarcode.TabIndex = 1;
+            // 
+            // pbBarcode
+            // 
+            this.pbBarcode.Location = new System.Drawing.Point(46, 59);
+            this.pbBarcode.Name = "pbBarcode";
+            this.pbBarcode.Size = new System.Drawing.Size(467, 208);
+            this.pbBarcode.TabIndex = 2;
+            this.pbBarcode.TabStop = false;
+            // 
+            // Generate_Barcode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 307);
+            this.Controls.Add(this.pbBarcode);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.button1);
+            this.Name = "Generate_Barcode";
+            this.Text = "Generate_Barcode";
+            this.Load += new System.EventHandler(this.Generate_Barcode_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbBarcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.PictureBox pbBarcode;
+    }
+}
---
+++ GodavariFoods/GodavariFoods/Generate_Barcode.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Aspose.BarCode;
+
+namespace GodavariFoods
+{
+    public partial class Generate_Barcode : Form
+    {
+        public Generate_Barcode()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate_Barcode_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ////Instantiate BarCodeBuilder object
+            //BarCodeBuilder bb = new BarCodeBuilder();
+
+            ////Set the Code text for the barcode
+            //bb.CodeText = "1234567";
+
+            ////Set the symbology type to Code128
+            //bb.SymbologyType = Symbology.Code128;
+
+            ////Set the width of the bars to 1 millimeters
+            //bb.xDimension = 1f;
+
+            ////Save the image to your system and set its image format to Jpeg
+            //bb.Save("barcode.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            string barcode = txtBarcode.Text.Trim();
+
+            Bitmap bitmap = new Bitmap(barcode.Length * 40, 50);
+
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                Font font = new Font("IDAutomationHC39M Free Version", 20);
+                PointF point = new PointF(2f, 2f);
+                SolidBrush black = new SolidBrush(Color.Black);
+                SolidBrush white=new SolidBrush(Color.White);
+
+                graphics.FillRectangle(white, 0, 0, bitmap.Width, bitmap.Height);
+                graphics.DrawString("*" + barcode + "*", font, black, point);
+            }
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Jpeg);
+                pbBarcode.Image = bitmap;
+                //pbBarcode.Width = bitmap.Width;
+                //pbBarcode.Height = bitmap.Height;
+            }
+
+
+        }
+
+    }
+}
